eaa975efa8579c85609b7a01e2123e9464e9c782
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-03-08 08:54:43 -0800

    last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+text = ""
+last_digit = number % 10
+if last_digit > 5:
+    text = "and is greater than 5"
+elif last_digit == 0:
+    text = "and is 0"
+elif last_digit < 6:
+    text = "and is less than 6 and not 0"
+print(f"Last digit of {number} is {last_digit} {text}")
