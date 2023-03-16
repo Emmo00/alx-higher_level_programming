111945bc0e422d801f2217bcd9a97a80af754b31
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-03-16 08:10:11 -0700

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,6 +3,5 @@ def uniq_add(my_list=[]):
     uniq = set(my_list)
     sum = 0
     for i in my_list:
-        if i not in uniq:
-           sum = sum + i
-    return sum 
+        sum = sum + i
+    return sum
