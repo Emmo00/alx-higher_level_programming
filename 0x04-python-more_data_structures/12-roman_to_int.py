93dcdba9421e1d8e28849b6571d79b108c21306d
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-03-16 15:48:51 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+def roman_to_int(roman_string:str):
+    con = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    number = 0
+    roman_numerals = list(roman_string)
+    for i in range(len(roman_numerals)):
+        if i == 0:
+            number = con[roman_numerals[i]]
+            continue
+        if con[roman_numerals[i]] <= con[roman_numerals[i - 1]]:
+            number = number + con[roman_numerals[i]]
+        elif con[roman_numerals[i]] > con[roman_numerals[i - 1]]:
+            number = con[roman_numerals[i]] - number
+    return number
+    
