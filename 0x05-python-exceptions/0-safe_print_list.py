7534132b8ccae2fe5d473edd9c209c43029d6920
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-27 21:40:56 +0100

    my bad

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     chars = 0
     try:
-        for _ in range(x):
+        for i in range(x):
             print(my_list[i], end="")
             chars += 1
     except NameError:
