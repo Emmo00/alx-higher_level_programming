92790ece7b65c48b0492024d755f02a9f6a8d221
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-29 21:54:26 +0100

    2 debug

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-class Square():
-    """A square class
-    
-    Args:
-        size: (integer) size of the square
-    Attributes:
-        size: private
-    """
-    def __init__(self, size=0):
+
+"""Define Square class"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """__init__: set size
+        
+        check if size is integer and less than 0
+
+        Args:
+            size: size of the square
+        Attributes:
+            size: private"""
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
