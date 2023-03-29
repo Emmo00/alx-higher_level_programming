132c7ed56999ec27afe4f9e80b650a1017bbed38
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-29 21:20:08 +0100

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+class Square():
+    """A square class
+    
+    Args:
+        size: size of the square
+    Attributes:
+        size: private
+    """
+    def __init__(self, size) -> None:
+        self.__size = size
