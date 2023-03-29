624601f582a5a249097ee2d48f7c9823f22fa71a
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-29 22:23:59 +0100

    5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Define Square class"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """initialize square, set size.
+
+        Args:
+            size (int): size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Area of square
+
+        Args:
+            no arguments
+        Return:
+            area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        """prints a square"""
+        if self.__size == 0:
+            print("")
+            return
+        for _ in range(self.__size):
+            for __ in range(self.__size):
+                print("#", end="")
+            print("")
