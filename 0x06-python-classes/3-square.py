bb82323f00431e4541142ec42961df2b009872d3
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-29 21:34:43 +0100

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -13,3 +13,13 @@ class Square():
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+        
+    def area(self):
+        """Area of square
+        
+        Args:
+            no arguments
+        Return:
+            area of the square
+        """
+        return self.__size ** 2
