38be220e0dabb0c1e7dcbcdd15270091bab63aa2
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-03-29 22:14:22 +0100

    4 setter getter

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
-class Square():
-    """A square class
-    
-    Args:
-        size: (integer) size of the square
-    Attributes:
-        size: private
-    """
-    def __init__(self, size=0):
-        if type(size) != int:
+"""Define Square class"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, size):
+        """initialize square, set size.
+
+        Args:
+            size (int): size of the square.
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-        
+
     def area(self):
         """Area of square
-        
+
         Args:
             no arguments
         Return:
             area of the square
         """
         return self.__size ** 2
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
