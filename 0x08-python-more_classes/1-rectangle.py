7a964e6043e79ef82b1dcc60a6ef75472fd569bf
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-03 12:24:38 -0700

    check init

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -7,6 +7,14 @@ class Rectangle:
     """does nothing"""
     def __init__(self, width=0, height=0):
         """initiate rectangle object"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
         self.__width = width
         self.__height = height
 
