64ad9d61fa5b6c5b99d211a88eeba88ff6cc19ec
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-04 10:52:35 -0700

    zero width

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,6 +51,8 @@ class Rectangle:
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
         string = ""
         for _ in range(self.__height):
             for __ in range(self.__width):
