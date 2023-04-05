372493c41251409415832833477335601b5291ae
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-05 03:03:13 +0100

    first

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+"""
+This is the add_integer module
+
+The add_integer module supplies one function, add_integer(). For example,
+
+>>> add_integer(2, 3)
+5
+"""
+
+def add_integer(a, b=98):
+    return a + b
+
