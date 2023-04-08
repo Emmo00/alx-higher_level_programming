19747d45398bfe94eeee8b80132f51edf1ddf3b8
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-08 13:29:40 +0100

    hope

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def say_my_name(first_name, last_name=""):
+    """prints My name is <first name> <last name>
+    Example
+    >>> say_my_name("Emmanuel", "Nwafor")
+    My name is Emmanuel Nwafor
+    """
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+    
