1f8d085334b08df2a180633255fe056701ecead8
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-08 19:58:22 +0100

    print_square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+print_square module hase one function:
+
+print_square()
+Example:
+    >>> print_square(4)
+    ####
+    ####
+    ####
+    ####
+"""
+
+
+def print_square(size):
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        for __ in range(size):
+            print("#", end="")
+        print("")
