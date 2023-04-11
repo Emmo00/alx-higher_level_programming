0cc0a59895d8ca6f0b55a946f9ed888668724bd9
emma <emma@emma.myguest.virtualbox.org>
2023-04-11 19:01:01 +0100

    1 list subclass

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Defines MyList class - a subclass of the list class"""
+
+
+class MyList(list):
+    """MyList - a subclass of the list class"""
+
+    def print_sorted(self):
+        tmp = []
+        for item in self:
+            tmp.append(item)
+        tmp.sort()
+        print(tmp)
+        del tmp
