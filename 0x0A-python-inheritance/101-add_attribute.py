9c394c9da43f7b874deec89123380ef3e4c22f6c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-11 16:01:07 -0700

    ok

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
+"""Definea add_attribute function
+"""
 
 
 def add_attribute(obj, name, value):
+    """adds attribute to an object if possible
+    else raise an exception
+    """
     if '__dict__' in dir(obj):
         setattr(obj, name, value)
     else:
