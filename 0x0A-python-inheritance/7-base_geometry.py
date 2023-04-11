182359c230538dd3b899a07a60da8c77663daec0
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-11 20:53:02 +0100

    integer validator with tests

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines an empty class:
+    BaseGeometry
+    """
+
+
+class BaseGeometry():
+    """An BaseGeometry class
+    """
+    def area(self):
+        """raises an error if the area
+        method is not redefined in a child class or
+        instance of this class
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates the value passed to it
+        - checks whether it is an integer
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
