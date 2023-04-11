3ef716fb13715c5b94c0ce05129f5c388d8bd343
emma <emma@emma.myguest.virtualbox.org>
2023-04-11 19:38:50 +0100

    add more doc string

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,12 +6,21 @@ Example:
 
 
 class MyList(list):
-    """MyList - a subclass of the list class"""
+    """MyList - a subclass of the list class
+    class methods:
+        print_sorted()
+        """
 
     def __init__(self, iterable=[]):
+        """initialize the MyList class
+        Uses init from base class `list`
+        """
         super().__init__(iterable)
 
     def print_sorted(self):
+        """ this method prints the list
+        but sorted (ascending order)
+        """
         tmp = []
         for item in self:
             tmp.append(item)
