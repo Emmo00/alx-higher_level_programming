fc02c4af9a8161cf625b4399dee183c1f89cbfec
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-12 16:51:44 -0700

    pascal

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+ #!/usr/bin/python3
+"""Define pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing the
+    Pascal’s triangle of n:
+    """
+    if n <= 0:
+        return []
+    triangle = []
+    for i in range(n):
+        t = [1] * (i + 1)
+        for j in range(i + 1):
+            if j + 1 == len(t) or j == 0:
+                continue
+            t[j] = triangle[i - 1][j] + triangle[i - 1][j - 1]
+        triangle.append(t)
+    return triangle
