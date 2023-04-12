13108ded45fd4dcc635f0b4c90e2514fa3efaf8a
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-04-12 14:48:43 +0100

    to json string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+"""Defines to_json_string function
+"""
+
+
+def to_json_string(my_obj):
+    """returns the JSON representation of
+    an object
+    """
+    return json.dumps(my_obj)
