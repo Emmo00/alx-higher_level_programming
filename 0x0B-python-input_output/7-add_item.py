e77260be2313db0e5cd9a7f8bef3a6b543960d3e
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-12 12:35:04 -0700

    add

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""adds all arguments to a Python list, and
+then save them to a file:
+"""
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+os = __import__('os')
+
+
+def main():
+    my_list = []
+    av = os.argv[1:]
+    for arg in av:
+        my_list.append(arg)
+    save_to_json_file(my_list, "add_item.json")
+
+
+if __name__ == '__file__':
+    main()
