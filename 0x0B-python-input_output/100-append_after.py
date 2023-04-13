acf4a643e42dd6136310b610f61edc28b744db26
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-04-12 17:41:27 -0700

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,7 +5,7 @@
 
 def append_after(filename="", search_string="", new_string=""):
     """inserts a line of text to a file, after
-    each line containing a specific string 
+    each line containing a specific string
     """
     if filename == "":
         return
