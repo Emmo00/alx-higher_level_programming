eff4f4b2432b71cdb7b736e35f86b650ca32d5b8
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-08 04:59:43 +0100

    arg

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+    from sys import argv
+    argc = len(argv)
+    print("{} argument{}{}".format(
+        argc - 1,
+        's' if argc != 2 else '',
+        '.' if argc < 2 else ':'
+        ))
+    if argc > 0:
+        for i in range(1, argc):
+            print("{}: {}".format(i, argv[i]))
