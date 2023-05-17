2e377eaaa8a407495230c9202fc14e711ee5dfeb
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-17 12:12:41 -0700

    best-score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- selects score and name columns from second_table table 
-SELECT score, name FROM second_table WHERE score >= 0 ORDER BY score DESC
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC
