74e12eb9e5930f7c701ef37740707a8c53bf8ba1
Emmanuel Nwafor <88244468+Emmo00@users.noreply.github.com>
2023-05-17 15:51:48 +0100

    Add missing column

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,5 +2,5 @@
 CREATE TABLE second_table IF NOT EXISTS (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALIES (1,  “John”, 10));
 INSERT INTO second_table (id, name, score) VALIES (2,   “Alex”, 3));
-INSERT INTO second_table (id, name, score) VALIES (3,  Bob”, 14));
+INSERT INTO second_table (id, name, score) VALIES (3,  "Bob", 14));
 INSERT INTO second_table (id, name, score) VALIES (4,  “George”, 8));
