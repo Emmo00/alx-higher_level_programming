7dd2996e7833751307dd2b8096c04b375c225b86
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-05-18 09:04:59 -0700

    hih

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- lists all privileges of the users user_0d_1 and user_od_2
-SHOW GRANTS 'user_0d_1'@localhost;
-SHOW GRANTS 'user_0d_2'@localhost;
+SELECT * FROM mysql.user
+WHERE user IN ('user_0d_1', 'user_0d_2')
+AND host = 'localhost';
