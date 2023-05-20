0be26a5ee4590a1d8b8f0e684a74788965317cf6
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-05-20 21:42:20 +0100

    tabs to space

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,33 +5,33 @@
  * linked list
  * @head: double pointer to head of the linked list
  * @number: data for the new node in the linked list
- * 
+ *
  * Return: pointer to the new node, or NULL if failed
 */
 listint_t *insert_node(listint_t **head, int number)
 {
-    listint_t *node, *tmp;
+	listint_t *node, *tmp;
 
-    if (head == NULL)
-        return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-    node = malloc(sizeof(listint_t));
-    if (node == NULL)
-        return (NULL);
-    node->n = number;
-    node->next = NULL;
-    if (*head == NULL)
-    {
-        *head = node;
-        return (node);
-    }
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+	node->n = number;
+	node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
 
-    tmp = *head;
-    while (tmp->next != NULL && tmp->next->n < number)
-    {
-        tmp = tmp->next;
-    }
-    node->next = tmp->next;
-    tmp->next = node;
-    return (node);
+	tmp = *head;
+	while (tmp->next != NULL && tmp->next->n < number)
+	{
+		tmp = tmp->next;
+	}
+	node->next = tmp->next;
+	tmp->next = node;
+	return (node);
 }
