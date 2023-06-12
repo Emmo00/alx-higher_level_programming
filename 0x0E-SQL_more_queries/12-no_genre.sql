dd8da4d5a10033745c40599abc46bd23ae070bf1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-12 06:07:20 -0700

    no genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,7 @@
--- list all shows contained in hbtn_0d_tvshows that have
+-- list all shows without a genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_show_genres
 RIGHT JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_show_genres.genre_id IS NULL
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id
