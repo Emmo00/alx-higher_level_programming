acccf293a5e0f6b752de174460d017ded203b270
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-13 14:20:16 +0100

    module.exports

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let timesToPrint = Number(process.argv[2])
 if (Number.isNaN(timesToPrint)) {
-    console.log('Missing number of occurences')
-    return
+  console.log('Missing number of occurrences')
+  return
 }
 for (let i = 0; i < timesToPrint; i++) {
-    console.log('C is fun')
+  console.log('C is fun')
 }
--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,6 +2,6 @@
 let a = Number(process.argv[2])
 let b = Number(process.argv[3])
 function add(a, b) {
-    return a + b
+  return a + b
 }
 console.log(add(a, b))
