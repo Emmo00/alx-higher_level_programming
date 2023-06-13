ebac01cd086b4f5a637546dea303387e06a1c605
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-13 13:07:13 +0100

    plural

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,6 +2,8 @@
 let argumentLength = process.argv.length
 if (argumentLength <= 2) {
     console.log('No argument')
+} else if (argumentLength === 3) {
+    console.log('Argument found')
 } else {
     console.log('Arguments found')
 }
