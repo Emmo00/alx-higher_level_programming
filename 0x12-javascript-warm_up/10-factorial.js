b596181c48144b4491147ed0ad733c154acea6a2
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-13 13:54:39 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+let number = Number(process.argv[2])
+function factorial(number) {
+    if (number === 0 || Number.isNaN(number)) return 1
+    return number * factorial(number - 1)
+}
+console.log(factorial(number))
