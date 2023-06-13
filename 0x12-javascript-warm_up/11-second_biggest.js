82754859710915dc1fa7d8601a6e4dca51964615
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-13 14:09:27 +0100

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let arguments = process.argv.slice(2)
+function secondBiggest(arr) {
+    let max = Math.max()
+    let secondBiggest
+    if (arr.length <= 1) return 0
+    for (let i of arr) {
+        if (!secondBiggest) secondBiggest = i
+        if (i < max && i > secondBiggest) secondBiggest = i
+    }
+    return secondBiggest
+}
+console.log(secondBiggest(arguments))
