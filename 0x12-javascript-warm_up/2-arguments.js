8055ab3000a14447104fa0caf32abfea6d2d6f65
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-13 13:31:46 +0100

    an integer

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let argumentLength = process.argv.length
 if (argumentLength <= 2) {
-    console.log('No argument')
+  console.log('No argument')
 } else if (argumentLength === 3) {
-    console.log('Argument found')
+  console.log('Argument found')
 } else {
-    console.log('Arguments found')
+  console.log('Arguments found')
 }
