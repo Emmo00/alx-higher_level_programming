6658ddc2fd601749958e2e7cd6f9b9cf1fa1a2f1
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-14 22:54:30 +0100

    wrong module name

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const dict = require('./100-data').dict;
+const dict = require('./101-data').dict;
 const sortedDict = {};
 for (const prop of Object.values(dict)) sortedDict[prop] = [];
 for (const [key, prop] of Object.entries(dict)) sortedDict[prop].push(key);
