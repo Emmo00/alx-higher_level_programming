d01b59f5ee2947f1d4aef86e36ccddaafc9b2b6c
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-18 00:37:48 +0100

    first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""list all State objects from the database passed
+as argument
+"""
+if __name__ == '__main__':
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    import sys
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        username,
+        password,
+        database))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    row = session.query(State).order_by(State.id).first()
+    print('{}: {}'.format(row.id, row.name))
