ac846cf22087be0aff543ed262c570834de36a70
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-17 23:43:23 +0100

    add docs

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -7,6 +7,9 @@ Base = declarative_base()
 
 
 class State(Base):
+    """State class
+    objects are mapped to MySQL rows in the states table
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
