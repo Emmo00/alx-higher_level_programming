251c63890dfcd6ff3eb4c1c2f4429a71e6162ef4
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-29 10:46:14 +0100

    start

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""makes a get request to https://alx-intranet.hbtn.io/status"""
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf8')}")
