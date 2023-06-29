f40640c0ed6d6a0d7363192a03192cc627712ed5
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-29 16:48:03 +0100

    git commit -m

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -7,8 +7,13 @@ if __name__ == '__main__':
     repo_name = sys.argv[1]
     owner = sys.argv[2]
     url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+    query_string = "?per_page=10"
     headers = {
-            
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version": "2022-11-28",
             }
-    res = requests.get(url, headers=headers)
-    commits = res.json
+    res = requests.get(url + query_string, headers=headers)
+    commits = res.json()
+    for commit in commits:
+        print(f"{commit.get('sha')}: "
+              f"{commit.get('commit').get('author').get('name')}")
