20ee5413ea0cb285abe83ee2d44979ab016f3f0a
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-29 04:38:20 -0700

    error check and post api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+"""sends a POST request to http://0.0.0.0:5000/search_user with the
+letter as a parameter."""
+if __name__ == '__main__':
+    import requests
+    import sys
 
+    q = ""
+    if len(sys.argv) >= 2:
+        q = sys.argv[1]
+    res = requests.post("http://0.0.0.0:5000/search_user", data={'q': q})
+    result = res.json()
+    if len(result) == 0:
+        print("No result")
+    else:
+        print(f"[{result.get('id')}] {result.get('name')}")
