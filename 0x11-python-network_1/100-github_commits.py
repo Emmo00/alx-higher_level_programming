ae4f45d17833d9fe28881a90dade426d562eda69
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-06-29 05:05:44 -0700

    git_another

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""list 10 commits"""
+if __name__ == '__main__':
+    import requests
+    import sys
 
+    repo_name = sys.argv[1]
+    owner = sys.argv[2]
+    url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
+    headers = {
+            
+            }
+    res = requests.get(url, headers=headers)
+    commits = res.json
