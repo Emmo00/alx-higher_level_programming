0a9d1bb462ee69b74c04fb40240bac311597c678
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-06-30 21:34:31 +0100

    peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,20 @@
 
 def find_peak(list_of_integers):
     """finds a peak in a list of unsorted integers"""
-    if len(list_of_integers) == 0:
-        return None
-    elif len(list_of_integers) in [1, 2]:
-        return max(list_of_integers)
-    return max(list_of_integers[1:-1])
+    arr = list_of_integers
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if (
+                (mid == 0 or arr[mid] >= arr[mid - 1])
+                and (mid == len(arr) - 1 or arr[mid] >= arr[mid + 1])
+                ):
+            return arr[mid]
+        elif mid > 0 and arr[mid] < arr[mid - 1]:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
