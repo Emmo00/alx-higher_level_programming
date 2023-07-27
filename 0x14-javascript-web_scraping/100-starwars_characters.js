85969db626638d1ce56cc7b9835b6e542b513186
Nwafor Emmanuel <nwaforemmanuel005@gmail.com>
2023-07-27 04:09:42 -0700

    characters

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const movieId = process.argv[2];
+const apiURL = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+request.get(apiURL, (err, res, data) => {
+  if (err) console.log(err);
+  for (const character of JSON.parse(data).characters) {
+    request.get(character, (error, res, data) => {
+      if (error) console.log(error);
+      console.log(JSON.parse(data).name);
+    });
+  }
+});
